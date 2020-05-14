fbaeb1627b6cf1f77e26d75e62a5af36ff9a1e79
petrioski <petri.karhunkoski@gmail.com>
2020-05-14 16:12:34 +0300

    perustoiminnallisuuden alkuosa (viikko 2)

---
+++ application/categories/views.py
@@ -0,0 +1,22 @@
+from application import app
+from application import db
+from flask import redirect, render_template, request, request, url_for
+from application.categories.models import Category
+
+
+@app.route("/categories", methods=["GET"])
+def categories_index():
+    return render_template("categories/list.html", categories = Category.query.all())
+
+@app.route("/categories/new/")
+def categories_form():
+    return render_template("categories/new.html")
+
+@app.route("/categories/", methods=["POST"])
+def categories_create():
+    cat = Category(request.form.get("name"))
+
+    db.session().add(cat)
+    db.session().commit()
+
+    return redirect(url_for("categories_index"))
