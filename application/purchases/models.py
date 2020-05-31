4722b59763af81b99318e7b740d86838b4b08359
petrioski <petri.karhunkoski@gmail.com>
2020-05-31 19:39:08 +0300

    lisätty foreign key ostoslista-luokkaan erillisen liitostaulun (tarpeeton) sijaan

--- application/purchases/models.py
+++ application/purchases/models.py
@@ -5,7 +5,7 @@ class Purchase(Base):
     amount = db.Column(db.Integer, nullable = False)
     collected = db.Column(db.Boolean, nullable = False)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-    shopping_lists = db.relationship("List", secondary="list_purchase")
+    list_id = db.Column(db.Integer, db.ForeignKey('list.id'), nullable=False)
 
 
     def __init__(self, amount):
