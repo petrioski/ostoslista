ee5f2f876e763bbc237494cc7b7db16099ade7f2
petrioski <petri.karhunkoski@gmail.com>
2020-05-31 19:45:59 +0300

    lisätty aloitussivu-templan valinta kirjautumistilanteen mukaan

--- application/views.py
+++ application/views.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from application import app
+from flask_login import current_user
+from application.lists.views import lists_index
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if current_user.is_authenticated:
+        return lists_index()
+    else:
+        return render_template("index.html")
