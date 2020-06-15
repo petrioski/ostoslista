be87fb832e87ab9434e014efb46641f924872e1e
petrioski <petri.karhunkoski@gmail.com>
2020-06-15 13:42:53 +0300

    uniikit kategoriat lisätty parametriksi ostoslista-templatelle

--- application/purchases/views.py
+++ application/purchases/views.py
@@ -1,28 +1,56 @@
 from flask import redirect, render_template, request, url_for
-from flask_login import login_required, current_user
+from flask_login import login_required
+from sqlalchemy.sql import text
 
 from application import app
 from application import db
 from application.purchases.models import Purchase
 from application.items.models import Item
 from application.purchases.forms import AddNewPurchaseForm
-# from application.categories.models import Category
-
-# TODO: formi itemille
 
 
 @app.route("/lists/<list_id>/purchases/add_mode/<add_mode>")
 @login_required
 def purchases_index(list_id, add_mode):
-    #add_mode = request.args.get('add_mode', default=0, type=int)
-    print("listan id " + list_id)
-    print(f"THIIIISIIS purchase index {add_mode}")
-    return render_template("purchases/list_contents.html", purchases=get_all_purchases(list_id), list_id=list_id, form=get_purchase_form(), add_mode=add_mode)
-    # return render_template("items/all_items.html")
+    all_purchases = get_all_purchases(list_id)
+    categories = get_categories(all_purchases)
+    return render_template(
+        "purchases/list_contents.html",
+        categories=categories,
+        purchases=all_purchases,
+        list_id=list_id,
+        form=get_purchase_form(),
+        add_mode=add_mode,
+    )
 
 
 def get_all_purchases(list_id):
-    return Purchase.query.filter_by(list_id=list_id).order_by(Purchase.item_id).all()
+    return (
+        Purchase.query.filter_by(list_id=list_id)
+        .order_by(Purchase.item_id)
+        .all()
+    )
+
+
+def get_categories(purchase_list):
+    unique_categories = set()
+    categories = []
+
+    for i in purchase_list:
+        stmt = text(
+            "SELECT category.name "
+            "FROM item "
+            "LEFT JOIN category "
+            " ON item.category_id = category.id "
+            "WHERE item.id = :id"
+        ).params(id=i.item_id)
+        res = db.engine.execute(stmt).first()
+        if not res[0] in unique_categories:
+            categories.append(res[0])
+            unique_categories.add(res[0])
+
+    return categories
+
 
 def get_purchase_form():
     items = db.session.query(Item).all()
@@ -36,11 +64,18 @@ def get_purchase_form():
 @app.route("/lists/<list_id>/purchases/add/<add_mode>", methods=["GET", "POST"])
 @login_required
 def create_purchase(list_id, add_mode):
-    print("TTTTTTTTTTT " + list_id)
     form = get_purchase_form()
-
+    all_purchases = get_all_purchases(list_id)
+    categories = get_categories(all_purchases)
     if request.method == "GET":
-        return render_template("purchases/add_purchases.html", form=form, list_id = list_id, purchases=get_all_purchases(list_id), add_mode=add_mode)
+        return render_template(
+            "purchases/add_purchases.html",
+            form=form,
+            list_id=list_id,
+            categories=categories,
+            purchases=all_purchases,
+            add_mode=add_mode,
+        )
 
     if request.method == "POST" and not form.validate_on_submit():
         return render_template("/purchases/add_purchases.html", form=form)
@@ -52,18 +87,45 @@ def create_purchase(list_id, add_mode):
     db.session().add(new_item)
     db.session().commit()
 
-    return redirect(url_for("create_purchase", list_id = list_id, add_mode=add_mode))
+    return redirect(
+        url_for("create_purchase", list_id=list_id, add_mode=add_mode)
+    )
 
 
-@app.route("/list/<list_id>/purchases/remove/<purchase_id>/add_mode/<add_mode>", methods=["POST"])
+@app.route(
+    "/list/<list_id>/purchases/remove/<purchase_id>/add_mode/<add_mode>",
+    methods=["POST"],
+)
 @login_required
 def delete_purchase(list_id, purchase_id, add_mode):
     db.session.query(Purchase).filter_by(id=purchase_id).delete()
     db.session.commit()
 
-    print(f"THIIIISIIS in delete {add_mode} ja vertailun tulos {add_mode == 1}")
-
     if add_mode == str(1):
-        return redirect(url_for("create_purchase", list_id = list_id, add_mode=add_mode))
+        return redirect(
+            url_for("create_purchase", list_id=list_id, add_mode=add_mode)
+        )
 
-    return redirect(url_for("purchases_index", list_id = list_id, add_mode=add_mode))
+    return redirect(
+        url_for("purchases_index", list_id=list_id, add_mode=add_mode)
+    )
+
+
+@app.route(
+    "/list/<list_id>/purchases/collected/<purchase_id>/add_mode/<add_mode>)",
+    methods=["POST"],
+)
+@login_required
+def collect_purchase(list_id, purchase_id, add_mode):
+    id = Purchase.query.get(purchase_id)
+
+    if id.collected is False:
+        id.collected = True
+    else:
+        id.collected = False
+
+    db.session().commit()
+
+    return redirect(
+        url_for("purchases_index", list_id=list_id, add_mode=add_mode)
+    )
