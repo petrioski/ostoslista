78569b334e240abb23636dabeb3c755ae6c732f3
petrioski <petri.karhunkoski@gmail.com>
2020-06-15 22:26:11 +0300

    admineille poisto-oikeus tuotekategorioihin

--- application/categories/views.py
+++ application/categories/views.py
@@ -3,14 +3,20 @@ from application import db
 from flask import redirect, render_template, request, url_for
 from application.categories.models import Category
 from application.categories.forms import CreateNewCategoryForm
-from flask_login import login_required
+from flask_login import current_user
+from application import login_required, login_manager
 
 
 @app.route("/categories", methods=["GET"])
 @login_required
 def categories_index():
+    admin = False
+    if current_user.role == "ADMIN":
+        admin = True
     return render_template(
-        "categories/list.html", categories=Category.find_items_per_category()
+        "categories/list.html",
+        categories=Category.find_items_per_category(),
+        admin=admin,
     )
 
 
@@ -36,3 +42,12 @@ def create_category():
 @app.route("/categories/new/")
 def categories_form():
     return render_template("categories/new.html", form=CreateNewCategoryForm())
+
+
+@app.route("/categories/remove/<category_id>/", methods=["POST"])
+@login_required(role="ANY")
+def delete_category(category_id):
+    db.session.query(Category).filter_by(id=category_id).delete()
+    db.session.commit()
+
+    return redirect(url_for("categories_index"))
