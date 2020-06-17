3dc68efc55be22f2b5300c09d58afa5b7258d0d8
petrioski <petri.karhunkoski@gmail.com>
2020-06-17 17:48:14 +0300

    cascade-muutoksen jälkeen poiskommentoitu koodi deletoitu

--- application/purchases/views.py
+++ application/purchases/views.py
@@ -37,48 +37,6 @@ def get_categories(purchase_list):
         return Purchase.get_postgres_categories(purchase_list)
     else:
         return Purchase.get_sqlite_categories(purchase_list)
-        # return get_sqlite_categories(purchase_list)
-
-
-# def get_postgres_categories(purchase_list):
-#     ids = [p.item_id for p in purchase_list]
-#     stmt = text(
-#         "SELECT category.name "
-#         " FROM item "
-#         " LEFT JOIN category "
-#         "  ON item.category_id = category.id "
-#         " WHERE item.id = ANY(:id) "
-#         " GROUP BY category.name "
-#     ).params(id=ids)
-
-#     res = db.engine.execute(stmt)
-
-#     categories = []
-#     for row in res:
-#         categories.append(row[0])
-
-#     return categories
-
-
-# def get_sqlite_categories(purchase_list):
-#     unique_categories = set()
-#     categories = []
-
-#     for i in purchase_list:
-#         stmt = text(
-#             "SELECT category.name "
-#             "FROM item "
-#             "LEFT JOIN category "
-#             " ON item.category_id = category.id "
-#             "WHERE item.id = :id"
-#         ).params(id=i.item_id)
-
-#         res = db.engine.execute(stmt).first()
-#         if not res[0] in unique_categories:
-#             categories.append(res[0])
-#             unique_categories.add(res[0])
-
-#     return categories
 
 
 def get_purchase_form():
@@ -90,7 +48,9 @@ def get_purchase_form():
     return form
 
 
-@app.route("/lists/<list_id>/purchases/add/<add_mode>", methods=["GET", "POST"])
+@app.route(
+    "/lists/<list_id>/purchases/add/<add_mode>", methods=["GET", "POST"]
+)
 @login_required
 def create_purchase(list_id, add_mode):
     form = get_purchase_form()
